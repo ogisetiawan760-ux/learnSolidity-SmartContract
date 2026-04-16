f437498ebf2a61a01ee165c2101fdbf56a3eb7de
ogisetiawan760-ux <ogisetiawan760@gmail.com>
2026-04-16 13:28:22 +0700

    Implement owner management and access control
    Added owner variable and onlyOwner modifier for access control.

--- FundMe.sol
+++ FundMe.sol
@@ -12,13 +12,19 @@ contract FundMe {
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmountFuded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd , "didn't send enough ETH");//1e18 = 1 ETH = 10000000000000000000 = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFuded[msg.sender] += msg.value;
     }
 
-    function withdraw()public {
+    function withdraw()public onlyOwner{
         for(uint256 funderIndex = 0;funderIndex < funders.length;funderIndex++){
             address funder   = funders[funderIndex];
             addressToAmountFuded[funder]= 0;
@@ -29,6 +35,12 @@ contract FundMe {
         (bool callSucces ,)= payable(msg.sender).call{value : address(this).balance}("");
         require(callSucces,"call failed");
     }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner , "Sender is not owner");
+        _;
+    }
+
 
   
 }
