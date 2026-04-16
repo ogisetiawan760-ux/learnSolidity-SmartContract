d2c9cd2d1349cf894c4ea098de8527c236e54480
ogisetiawan760-ux <ogisetiawan760@gmail.com>
2026-04-16 10:11:33 +0700

    Fix funding logic and add withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -6,6 +6,7 @@ import{PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
     using PriceConverter for uint256; 
+
     uint256 public minimumUsd = 5e18;
 
     address[] public funders;
@@ -14,10 +15,20 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd , "didn't send enough ETH");//1e18 = 1 ETH = 10000000000000000000 = 1 * 10 ** 18
         funders.push(msg.sender);
-        addressToAmountFuded[msg.sender]=addressToAmountFuded[msg.sender] + msg.value;
+        addressToAmountFuded[msg.sender] += msg.value;
     }
 
-    
+    function withdraw()public {
+        for(uint256 funderIndex = 0;funderIndex < funders.length;funderIndex++){
+            address funder   = funders[funderIndex];
+            addressToAmountFuded[funder]= 0;
+
+        }
+        //riset the array
+        funders = new address[](0);
+        (bool callSucces ,)= payable(msg.sender).call{value : address(this).balance}("");
+        require(callSucces,"call failed");
+    }
 
   
 }
